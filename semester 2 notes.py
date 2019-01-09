3ddc0e0c1b91bcd9c68279ceed79bb446ebeb51d
1234567y <44381274+1234567y@users.noreply.github.com>
2019-01-09 14:08:06 -0800

    notes

--- semester 2 notes.py
+++ semester 2 notes.py
@@ -1,12 +1,12 @@
 print("hello world")
-
 # ooga booga
-
 car = 5
 driving = True
-
 print("I have %d cars" % car)
-
 input("How old are you?")
-
-colors = ["green, blue, red, black, white"]
+colors = ["green", "blue", "red", "black", "white"]
+print(colors)
+colors.append("yellow")
+colors.pop(2)
+print(colors[2])
+print(len(colors))
