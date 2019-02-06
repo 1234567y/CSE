2c0df8fcc812020758feb72dc200b8e34f6f1063
1234567y <44381274+1234567y@users.noreply.github.com>
2019-02-06 14:35:29 -0800

    Adventure game

--- Adventure game -Leila Taito.py
+++ Adventure game -Leila Taito.py
@@ -1,8 +1,7 @@
-import random
 lives = 1
 food = 0
 Food = ["Pizza", "Fries", "Poison berry"]
-
+playing = True
 Cities = {
     "LA",
     "San Fransisco",
@@ -20,4 +19,37 @@ Cities = {
     "Madera",
     "Riverdale"
 }
-print(Cities)
+
+areas = {
+    'Fresno': {
+        'NAME': "Desert",
+        'DESCRIPTION': "This room will contain an object, look for it",
+        'PATHS': {
+            "N": "Gaston",
+            "E": "LA",
+            "S": "Back room",
+            "W": "Kerman"
+
+        }
+    }
+}
+
+
+Moves = {
+    "North": "N",
+    "East": "E",
+    "South": "S",
+    "West": "W",
+    "Pick up": "P",
+    "Attack": "A"
+}
+
+while playing:
+    command = input("_>")
+    if command.lower()in ['q', 'quit', 'exit']:
+        playing = False
+    if command == Moves["North"]:
+        print("You are in Fresno")
+        print(areas["Fresno"]["DESCRIPTION"])
+
+
