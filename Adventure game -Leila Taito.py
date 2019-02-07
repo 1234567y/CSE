fff1548dc1e8daf53ee5b2877556d67278bfee43
1234567y <44381274+1234567y@users.noreply.github.com>
2019-02-07 14:35:58 -0800

    adventure games

--- Adventure game -Leila Taito.py
+++ Adventure game -Leila Taito.py
@@ -1,8 +1,7 @@
 lives = 1
 food = 0
 Food = ["Pizza", "Fries", "Poison berry"]
-playing = True
-Cities = {
+City_map = {
     "LA",
     "San Fransisco",
     "Washington",
@@ -22,18 +21,56 @@ Cities = {
 
 areas = {
     'Fresno': {
-        'NAME': "Desert",
+        'NAME': "Fresno",
         'DESCRIPTION': "This room will contain an object, look for it",
         'PATHS': {
-            "N": "Gaston",
-            "E": "LA",
-            "S": "Back room",
-            "W": "Kerman"
+            'West': "Kerman"
 
         }
+    },
+    'Kerman': {
+        'NAME': "Kerman",
+        'DESCRIPTION': "Here you can get to LA",
+        'PATHS': {
+            'North': "LA"
+        }
+    },
+    'LA': {
+      'NAME': "LA",
+      'DESCRIPTION': "From here you go to San Fransisco",
+      'PATHS': {
+            'South': "San Fransisco"
+        }
+    },
+    'San Fransisco': {
+        'NAME': "San Fransisco",
+        'DESCRIPTION': "From here you go to Washington",
+        'PATHS': {
+            'East': "Washington"
+        }
+    },
+    'Washington': {
+        'NAME': "Washington",
+        'DESCRIPTION': "From here you go to Oregon",
+        'PATHS': {
+            'East': "Oregon"
+        }
+    },
+    'Oregon': {
+        'NAME': "Oregon",
+        'DESCRIPTION': "From here you go to Mexico",
+        'PATHS': {
+                'South': "Mexico"
+            }
+    },
+    'Mexico': {
+        'NAME': "Mexico",
+        'DESCRIPTION': "From here you go to Antioch",
+        'PATHS': {
+            'North': "Antioch"
+        }
+    },
     }
-}
-
 
 Moves = {
     "North": "N",
@@ -43,13 +80,19 @@ Moves = {
     "Pick up": "P",
     "Attack": "A"
 }
-
+position = areas['Fresno']
+playing = True
 while playing:
+    # print("Test")
+    print(position["NAME"])
     command = input("_>")
-    if command.lower()in ['q', 'quit', 'exit']:
+    if command.lower() in ['q', 'quit', 'exit']:
         playing = False
-    if command == Moves["North"]:
-        print("You are in Fresno")
-        print(areas["Fresno"]["DESCRIPTION"])
-
-
+    elif command in Moves:
+        try:
+            room_name = position["PATHS"][command]
+            position = areas[room_name]
+        except KeyError:
+            print("I can't go that way.")
+    else:
+        print("Command not recognized.")
