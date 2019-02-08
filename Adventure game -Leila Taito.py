bd6e5e39ec15cb409ff6917e78715b49afb90dc4
1234567y <44381274+1234567y@users.noreply.github.com>
2019-02-08 14:30:42 -0800

    Adventure game 2/8

--- Adventure game -Leila Taito.py
+++ Adventure game -Leila Taito.py
@@ -24,7 +24,8 @@ areas = {
         'NAME': "Fresno",
         'DESCRIPTION': "This room will contain an object, look for it",
         'PATHS': {
-            'West': "Kerman"
+            'West': "Kerman",
+            'North': "Riverdale"
 
         }
     },
@@ -32,42 +33,112 @@ areas = {
         'NAME': "Kerman",
         'DESCRIPTION': "Here you can get to LA",
         'PATHS': {
-            'North': "LA"
+            'North': "LA",
+            'West': "Fresno"
         }
     },
     'LA': {
       'NAME': "LA",
       'DESCRIPTION': "From here you go to San Fransisco",
       'PATHS': {
-            'South': "San Fransisco"
+            'South': "San Fransisco",
+            'North': "LA"
         }
     },
     'San Fransisco': {
         'NAME': "San Fransisco",
         'DESCRIPTION': "From here you go to Washington",
         'PATHS': {
-            'East': "Washington"
+            'East': "Washington",
+            'South': "LA"
         }
     },
     'Washington': {
         'NAME': "Washington",
         'DESCRIPTION': "From here you go to Oregon",
         'PATHS': {
-            'East': "Oregon"
+            'West': "Oregon",
+            'East': "San Fransisco"
         }
     },
     'Oregon': {
         'NAME': "Oregon",
         'DESCRIPTION': "From here you go to Mexico",
         'PATHS': {
-                'South': "Mexico"
+                'South': "Mexico",
+                'West': "Washington"
             }
     },
     'Mexico': {
         'NAME': "Mexico",
         'DESCRIPTION': "From here you go to Antioch",
         'PATHS': {
-            'North': "Antioch"
+            'North': "Antioch",
+            'South': "Oregon"
+        }
+    },
+    'Antioch': {
+        'NAME': "Antioch",
+        'DESCRIPTION': "From here you go to Daily City",
+        'PATHS': {
+            'East': "Daily City",
+            'North': "Mexico"
+        }
+    },
+    'Daily City': {
+        'NAME': "Daily City",
+        'DESCRIPTION': "From here you go to Clovis",
+        'PATHS': {
+            'South': "Clovis",
+            'East': "Antioch"
+        }
+    },
+    'Clovis': {
+        'NAME': "Clovis",
+        'DESCRIPTION': "From here you go to Selma",
+        'PATHS': {
+            'East': "Selma",
+            'South': "Daily City"
+        }
+    },
+    'Selma': {
+        'NAME': "Selma",
+        'DESCRIPTION': "From here you go to Hanford",
+        'PATHS': {
+            'North': "Hanford",
+            'East': "Clovis"
+        }
+    },
+    'Hanford': {
+        'NAME': "Hanford",
+        'DESCRIPTION': "From here you go to Dinuba",
+        'PATHS': {
+            'West': "Dinuba",
+            'North': "Selma"
+        }
+    },
+    'Dinuba': {
+        'NAME': "Dinuba",
+        'DESCRIPTION': "From here you go to Madera",
+        'PATHS': {
+            'South': "Madera",
+            'West': "Hanford"
+        }
+    },
+    'Madera': {
+        'NAME': "Madera",
+        'DESCRIPTION': "From here you go to Riverdale",
+        'PATHS': {
+            'West': "Riverdale",
+            'South': "Dinuba"
+        }
+    },
+    'Riverdale': {
+        'NAME': "Riverdale",
+        'DESCRIPTION': "From here you will go to Fresno",
+        'PATHS': {
+            'North': "Fresno",
+            'West': "Madera"
         }
     },
     }
