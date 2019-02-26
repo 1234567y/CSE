6b0471205c7f95396156d698b9df9a53def60e29
1234567y <44381274+1234567y@users.noreply.github.com>
2019-02-26 14:35:01 -0800

    stufff

--- WorldmapOOP.py
+++ WorldmapOOP.py
@@ -7,6 +7,28 @@ class Room(object):
         self.south = south
 
 
+class Player(object):
+    def __init__(self, starting_location):
+        self.health = 100
+        self.inventory = []
+        self.current_location = starting_location
+
+    def move(self, new_location):
+        """ This moves a player to a new location
+
+        :param new_location: The room object that we move to
+        """
+        self.current_location = new_location
+
+    def find_room(self, direction):
+        """This method takes a direction, and finds the variable of the room.
+
+        :param direction: A string (all lowercase), with a cardinal direction
+        :return: A room object if it exists, None if it does not
+        """
+        return getattr(self.current_location, direction)
+
+
 # These are the instances of the rooms (Instantiation)
 
 # Option 1 - Use the Variables, but fix later
@@ -17,3 +39,25 @@ R19A.north = parking_lot
 # Option 2 - Use Strings, but more difficult controller
 R19A = Room("Mr. Wiebe's Room", 'parking_lot')
 parking_lot = Room("The parking lot", None, R19A)
+
+player = Player(R19A)
+
+directions = ['north', 'south', 'east', 'west', 'up', 'down']
+playing = True
+
+# Controller
+while playing:
+    print(player.current_location.name)
+    
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command in directions:
+        try:
+            room_name = current_node["PATHS"][command]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way.")
+    else:
+        print("Command not recognized.")
+
