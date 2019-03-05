49277cb7265f193874c6fb235be0616e3dfb4dbf
1234567y <44381274+1234567y@users.noreply.github.com>
2019-03-05 14:36:37 -0800

    Items

--- Items-Leila Taito.py
+++ Items-Leila Taito.py
@@ -1,1 +1,8 @@
-class Item
+class Item(object):
+    def __init__(self, name):
+        self.name = name
+
+
+class Treasure(Item):
+    def __init__(self, name):
+        super(Treasure, self).__init__(name)
