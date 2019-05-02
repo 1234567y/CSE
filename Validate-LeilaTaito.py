d7aa7582c7c5c636792ae100e584c6a083cabbb5
1234567y <44381274+1234567y@users.noreply.github.com>
2019-05-02 14:11:08 -0700

    5/2/19

--- Validate-LeilaTaito.py
+++ Validate-LeilaTaito.py
@@ -1,25 +1,25 @@
-test_num = "2067233931768570"
-string1 = "2067233931768570"
-list1 = list(string1)
+test_num = "4532450527702225"
+list1 = list(test_num)
 print(list1)
-print(len(list1))
 
 
 def validate(num: str):
     if len(list1) is not 16:
         print("does not pass.")
-
+        return False
+    last_num = int(num[15])
     new_list = list1[0:15][::-1]
     print(new_list)
     for index in range(len(new_list)):
         new_list[index] = int(new_list[index])
         if index % 2 == 0:
             new_list[index] *= 2
-            if new_list[index] >= 9:
+            if new_list[index] > 9:
                 new_list[index] -= 9
-    newer_list = sum(new_list)
-    print(newer_list)
-    print(71 % 10)
+    print(new_list)
+    total = sum(new_list)
+    print(total)
+    return total % 10 == last_num
 
 
 print(validate(test_num))
