e711a3a24caa638c20b845bb89bce772ec76ecef
1234567y <44381274+1234567y@users.noreply.github.com>
2019-05-03 14:47:22 -0700

    5/3/19

--- Validate-LeilaTaito.py
+++ Validate-LeilaTaito.py
@@ -1,15 +1,17 @@
-test_num = "4532450527702225"
-list1 = list(test_num)
-print(list1)
+import csv
+test_num = "5337843058931904"
+# list1 = list(test_num)
+# print(list1)
 
 
 def validate(num: str):
-    if len(list1) is not 16:
+    if len(num) is not 16:
         print("does not pass.")
         return False
     last_num = int(num[15])
-    new_list = list1[0:15][::-1]
+    new_list = list(num)[:15][::-1]
     print(new_list)
+    print(last_num)
     for index in range(len(new_list)):
         new_list[index] = int(new_list[index])
         if index % 2 == 0:
@@ -23,3 +25,6 @@ def validate(num: str):
 
 
 print(validate(test_num))
+
+
+with open("Book1.csv", 'r') as test_num:
