fe9ab1514e74bc4621ec231e68d51b1d8ff44778
1234567y <44381274+1234567y@users.noreply.github.com>
2019-05-08 14:38:54 -0700

    5/8/19

--- num.py
+++ num.py
@@ -1,8 +1,12 @@
 import csv
 
 
-def validate(num: str):
-    with open("Sales Records.csv", 'r') as old_csv:
-        reader = csv.reader(old_csv)
-        for row in reader:
-
+with open("Sales Records.csv", 'r') as old_csv:
+    reader = csv.reader(old_csv)
+    total = 0
+    for row in reader:
+        old_number = row[13]
+        if row[2] == "Fruits":
+            num = float(old_number)
+            total = 
+            print(total)
