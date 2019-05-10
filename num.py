8dbd6df5c37606f7d57029a27a25d3a2b4873743
1234567y <44381274+1234567y@users.noreply.github.com>
2019-05-10 14:48:47 -0700

    5/10/19

--- num.py
+++ num.py
@@ -17,51 +17,78 @@ with open("Sales Records.csv", 'r') as old_csv:
     totalCE = 0
     for row in reader:
         old_number = row[13]
+
         if row[2] == "Fruits":
             num = float(old_number)
             totalF += num
+
         if row[2] == "Clothes":
             num = float(old_number)
             totalC += num
+
         if row[2] == "Beverages":
             num = float(old_number)
             totalB += num
+
         if row[2] == "Meat":
             num = float(old_number)
             totalM += num
+
         if row[2] == "Office Supplies":
             num = float(old_number)
             totalO += num
+
         if row[2] == "Cosmetics":
             num = float(old_number)
             totalCm += num
+
         if row[2] == "Snacks":
             num = float(old_number)
             totalS += num
+
         if row[2] == "Personal Care":
             num = float(old_number)
             totalPC += num
+
         if row[2] == "Vegetables":
             num = float(old_number)
             totalV += num
+
         if row[2] == "Household":
             num = float(old_number)
             totalH += num
+
         if row[2] == "Baby Food":
             num = float(old_number)
             totalBF += num
+
         if row[2] == "Cereal":
             num = float(old_number)
             totalCE += num
-print('Fruits = ', round(totalF, 2))
-print('Clothes = ', round(totalC, 2))
-print('Beverages = ', round(totalB, 2))
-print('Meat = ', round(totalM, 2))
-print('Office Supplies = ', round(totalO, 2))
-print('Cosmetics = ', round(totalCm, 2))
-print('Snacks = ', round(totalS, 2))
-print('Personal Care = ', round(totalPC, 2))
-print('Vegetables = ', round(totalV, 2))
-print('Household = ', round(totalH, 2))
-print('Baby Food = ', round(totalBF, 2))
-print('Cereal = ', round(totalCE, 2))
+Fruits = round(totalF, 2)
+Clothes = round(totalC, 2)
+Beverages = round(totalB, 2)
+Meat = round(totalM, 2)
+Office_Supplies = round(totalO, 2)
+Cosmetics = round(totalCm, 2)
+Snacks = round(totalS, 2)
+Personal_Care = round(totalPC, 2)
+Vegetables = round(totalV, 2)
+Household = round(totalH, 2)
+Baby_Food = round(totalBF, 2)
+Cereal = round(totalCE, 2)
+Item = [Fruits, Clothes, Beverages, Meat, Office_Supplies, Cosmetics, Snacks, Personal_Care, Vegetables, Household,
+        Baby_Food, Cereal]
+print("Fruits =", Fruits)
+print('Clothes =', Clothes)
+print('Beverages =', Beverages)
+print('Meat =', Meat)
+print('Office Supplies =', Office_Supplies)
+print('Cosmetics =', Cosmetics)
+print('Snacks =', Snacks)
+print('Personal Care =', Personal_Care)
+print('Vegetables =', Vegetables)
+print('Household =', Household)
+print('Baby Food =', Baby_Food)
+print('Cereal =', Cereal)
+print("The one with the highest profit is", max(Item))
